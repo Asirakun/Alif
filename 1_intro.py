899d885647a267f2e336c2a856d3a70c43dc0c1d
Asirakun <alifsikyridhofa11@gmail.com>
2024-09-03 12:34:23 +0700

    Perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,23 @@
+# coba coba
+print("Hello World")
+a = float(10.5)
+b = 5
+c = a * b
+
+# operasi (Operation) -> tambah (+), kurang(-), bagi(/), kali(*), modulo(%), pangkat(**)
+print(a)
+print(b)
+print(c)
+
+perkalian = a * b
+pembagian = a / b 
+pengurangan = a - b 
+modulo = a % b
+pangkat = a ** b
+
+print(perkalian)
+print(pembagian)
+print(pengurangan)
+print(modulo)
+print(pangkat)
+
